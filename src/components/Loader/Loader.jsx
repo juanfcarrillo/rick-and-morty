0b9df9b98506f92dc307982b1976cc295793c7c1
juanfi444 <78522923+juanfi444@users.noreply.github.com>
2022-09-25 17:40:00 -0500

    fix(loader): removed portal

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,8 @@
 import './styles/Loader.css'
 import spinImage from '../../assets/images/portal-loading.png'
-import { createPortal } from 'react-dom'
 
 const Loader = () => {
-  const loaderDOM = document.querySelector('#loader')
-
-  return createPortal(<img src={spinImage} alt="Loading" className='image-loader'/>, loaderDOM)
+  return <img src={spinImage} alt="Loading" className='image-loader'/>
 }
 
 export default Loader
